6112c2863ef81a000b7243ddf67adf323dc7243b
ljy-effort <3386579017@qq.com>
2024-12-06 16:19:03 +0800

    第三次提交，成功将Oracle数据库内容输出到页面

--- wms/src/main/java/com/wms/entity/Material.java
+++ wms/src/main/java/com/wms/entity/Material.java
@@ -7,7 +7,23 @@ import com.baomidou.mybatisplus.annotation.TableName;
 public class Material {
     @TableId
     private Long id;
-    private String code;
-    private String name;
+    private String materialCode; // 修改字段名为materialCode
+    private String materialName; // 修改字段名为materialName
     // 省略其他字段和getter/setter方法
+    // 添加getter和setter方法
+    public String getMaterialCode() {
+        return materialCode;
+    }
+
+    public void setMaterialCode(String materialCode) {
+        this.materialCode = materialCode;
+    }
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
 }
